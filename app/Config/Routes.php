5abf423b778678e343270dd83fe9802ca0cdd7c7
Muhammad Ilham <mhmmdilhamriza03@gmail.com>
2026-09-15 15:58:49 +0700

    Menambahkan menu siswa untuk projek website sekolah

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,3 +1,6 @@
+<!-- File ini berfungsi sebagai penghubung antara URL yang diakses user dengan Controller dan Method yang akan dijalankan. -->
+
+
 <?php
 
 use CodeIgniter\Router\RouteCollection;
@@ -5,49 +8,95 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// Ketika user membuka: http://localhost:8080/
+// Jalankan:app/Controllers/LoginController.php method:index()
+// Method index() nantinya akan menampilkan halaman login.
 $routes->get('/', 'LoginController::index');
 
 $routes->get('/login', 'LoginController::index');
+
+// Dashboard hanya boleh diakses oleh user yang sudah login
+// ['filter' => 'auth']
+// terhubung dengan:
+// app/Filters/AuthFilter.php
 $routes->get(
     '/dashboard', 
     'DashboardController::index',
     ['filter' => 'auth']);
+
+// Route untuk memproses login Dipanggil ketika user menekan tombol Login
+// pada file: app/Views/auth/login.php Karena form login menggunakan method="post"
+// maka route juga harus menggunakan post()
+// Method yang dijalankan LoginController::process()
 $routes->post(
     '/login/process',
     'LoginController::process'
 );
+
+// Ketika user klik tombol Logout maka Jalankan LoginController::logout() untuk menghapus seluruh session login.
 $routes->get(
     '/logout',
     'LoginController::logout'
 );
+
+// Menampilkan daftar seluruh user.
+// URL: /users Controller: UserController
+// Method: index()
 $routes->get(
     '/users',
     'UserController::index',
     ['filter' => 'auth']
 );
+
+// Menampilkan form tambah user.
+// Tidak menyimpan data.
+// Hanya menampilkan app/Views/users/create.php
 $routes->get(
     '/users/create',
     'UserController::create',
     ['filter' => 'auth']
 );
+
+// Menyimpan data user baru.
+// Dipanggil ketika tombol Simpan pada users/create.php ditekan.
+// Method yang dijalankan: UserController::store()
 $routes->post(
     '/users/store',
     'UserController::store',
     ['filter' => 'auth']
 );
 
+// Menampilkan form edit user.
+// (:num) berarti menerima angka.
+//
+// Contoh:/users/edit/5
+// akan menjadi: UserController::edit(5)
+//
+// Angka 5 akan diterima sebagai parameter $id.
 $routes->get(
     '/users/edit/(:num)',
     'UserController::edit/$1',
     ['filter' => 'auth']
 );
 
+// Menyimpan hasil edit user.
+//
+// Contoh: /users/update/5
+//
+// akan menjalankan: update(5)
+//
+// lalu mengupdate data user dengan id = 5.
 $routes->post(
     'users/update/(:num)',
     'UserController::update/$1',
     ['filter' => 'auth']
 );
 
+// Menghapus user berdasarkan id.
+// Contoh: /users/delete/5
+// akan menjalankan: delete(5)
+// kemudian menghapus user dengan id = 5.
 $routes->get(
     '/users/delete/(:num)',
     'UserController::delete/$1',
--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -1,11 +1,22 @@
 <?php 
 
+// Menentukan bahwa LoginController berada di folder: app/Controllers
+// Namespace digunakan agar CodeIgniter mengetahui lokasi class ini.
+// LoginController merupakan controller yang bertanggung jawab
+// untuk seluruh proses autentikasi (Login & Logout)
+
 namespace App\Controllers;
 
+// Menggunakan UserModel
+// File yang dipanggil: app/Models/UserModel.php
+// Tujuan: Agar LoginController dapat mengambil data user dari database.
 use App\Models\UserModel;
 
 class LoginController extends BaseController
 {
+
+    // Method index() bertugas menampilkan halaman login.
+    // Method ini dipanggil oleh: Routes.php
     public function index()
     {
        return view('auth/login');
@@ -13,46 +24,78 @@ class LoginController extends BaseController
 
     public function process()
     {
+
+        // Mengambil data yang dikirim dari form login.
+        // Data berasal dari: app/Views/auth/login.php
+        // Karena form menggunakan: method="POST" maka menggunakan: getPost()
         $username = $this->request->getPost('username');
         $password = $this->request->getPost('password');
 
+        // Membuat object UserModel.
+        // File: app/Models/UserModel.php
+        // Tujuan: Agar LoginController dapat mengambil data dari tabel users.
         $userModel = new UserModel();
 
+        // Mencari data user berdasarkan username yang diinput.
+        // SQL yang kira-kira dijalankan:
+        // SELECT * FROM users WHERE username = '$username' LIMIT 1
         $user = $userModel 
             ->where('username', $username)
             ->first();
         
+        // Mengecek apakah username
+        // ditemukan di database.
+        // Jika: $user = null berarti username tidak ada.
         if (!$user) {
 
+        // Jika username tidak ditemukan: Kembali ke halaman login.
+        // Mengirim Flash Message: Username tidak ditemukan
+        // Pesan ini nanti ditampilkan pada: app/Views/auth/login.php
         return redirect()
             ->to('/login')
             ->with('error', 'Username tidak ditemukan');
         }
 
+        // Mengecek apakah password yang diinput user sama dengan password yang tersimpan di database.
         if (
             !password_verify(
                 $password,
                 $user['password']
         ))
+
+        // Jika password salah: Kembali ke halaman login.
+        // Mengirim Flash Message: Password salah
+        // View: auth/login.php akan menampilkan pesan tersebut.
         {
         return redirect()
             ->to('login')
             ->with('error', 'Password salah');
         }
 
+        // Menyimpan informasi user ke dalam Session.
+        // Session ini digunakan sebagai penanda bahwa user sudah login.
+        // File yang menggunakan session ini: app/Filters/AuthFilter.php
+        // AuthFilter akan mengecek:session('isLoggedIn')
+        // Jika TRUE:user boleh masuk Dashboard.
+        // Jika FALSE: user diarahkan ke Login.
         session()->set([
             'user_id' => $user['id'],
             'username' => $user['username'],
             'isLoggedIn' => true
         ]);
 
+        // Setelah login berhasil user diarahkan ke: /dashboard
         return redirect()->to('/dashboard');
     }
 
+    // Method logout() digunakan
+    // Route: /logout berasal dari: Routes.php
     public function logout()
     {
+        // Menghapus seluruh Session.
         session()->destroy();
 
+        // Setelah logout selesai,user dikembalikan ke halaman login.
         return redirect()->to('/login');
     }
 }
